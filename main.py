1f06f9249494553443ee27243ade6628512202cd
karthik <karthik.ayangar7118@gmail.com>
2023-02-21 23:54:48 +0530

    added homescreen

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import pygame
+import sys
 from sys import exit 
 from pygame import mixer
 from random import randint
-
+import os
 
 # Initializing pygame and mixer
 pygame.init()
@@ -111,7 +112,7 @@ for floor in range(MAX_FLOORS):
 
 floor_rect_list = []
 
-print(screen.get_width())
+# print(screen.get_width())
 # floor timer
 # FLOOR_TIMER = pygame.USEREVENT + 1
 # pygame.time.set_timer(FLOOR_TIMER, 1)
@@ -125,33 +126,124 @@ player_rect = player_surf.get_rect(midbottom = (100,WIN_HEIGHT-floor_surf.get_he
 # Background music
 mixer.Channel(0).play(mixer.Sound('music/gameBG.mp3'), loops= 100)
 # mixer.Channel(1).play(mixer.Sound('music/theme.mp3'), loops= 100)
-mixer.Channel(0).set_volume(0.05)
+mixer.Channel(0).set_volume(0.01)
 # mixer.Channel(1).set_volume(0)
 
 jump_counter = 0
+
+def welcome(screen):
+    # WIN_HEIGHT= 900
+    # WIN_WIDTH = 700
+    SCREEN = screen
+    pygame.display.set_caption("IcyTower")
+    tilt = 0 
+    turn = True
+    font = TEXT_FONT
+    font.bold = True
+    font.italic = False
+    active = True
+    menu = ["PlayGame", "Instruction", "Profile", "HighScore", "LoadReplay", "Options", "Exit"]
+    index = 0
+    menu_dict = {
+         "PlayGame" : True,
+         "Instruction" : False,
+         "Profile" : False,
+         "HighScore" : False,
+         "LoadReplay" : False,
+         "Options" : False,
+         "Exit" : False 
+    }
+    clock = pygame.time.Clock()
+    temp_im = pygame.image.load(os.path.join("sprites", "icyMan.png"))
+    im = pygame.transform.rotozoom(temp_im,0, 0.5)
+
+    menu_img = pygame.image.load(os.path.join("sprites", "menu.png"))
+    menu_img = pygame.transform.rotozoom(menu_img, 0, 1.3)
+    menu_rect = menu_img.get_rect(center=(WIN_WIDTH//2 + 150, WIN_HEIGHT//2 + 150))
+
+    hand_img = pygame.image.load(os.path.join("sprites", "hand.png"))
+    hand_img = pygame.transform.rotozoom(hand_img, 0, 0.6)
+
+    hand_rect = hand_img.get_rect(center=(WIN_WIDTH//2 - 18, WIN_HEIGHT//2 +  80  ))
+
+    logo_im = pygame.image.load(os.path.join("sprites","homescreen1.png")).convert_alpha()
+    logo_im = pygame.transform.rotozoom(logo_im, 0, 1.2)
+    logo_rect = logo_im.get_rect(center=(WIN_WIDTH//2, WIN_HEIGHT//2 - 130))
+
+    while active:
+        SCREEN.fill((0,0,0))
+        SCREEN.blit(background_surf, (0,0))
+        SCREEN.blit(logo_im, logo_rect)
+        rot_image = pygame.transform.rotate(im, tilt)
+        new_rect = rot_image.get_rect(center=(WIN_WIDTH//2 - 220, WIN_HEIGHT//2 + 150))
+
+        SCREEN.blit(rot_image, new_rect)
+        SCREEN.blit(menu_img, menu_rect)
+        SCREEN.blit(hand_img, hand_rect)
+
+        if turn:
+            tilt += .5
+            if tilt == 40:
+                turn = False
+        else:
+            tilt -= .5
+            if tilt == -40:
+                turn = True
+        
+
+        for event in pygame.event.get():
+            if (event.type == pygame.QUIT)  :
+                pygame.quit()
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    pygame.quit()
+                    sys.exit()
+                if event.key == pygame.K_DOWN and index<(len(menu)-1):
+                    hand_rect.y += 28.5
+                    menu_dict[menu[index]] = False
+                    menu_dict[menu[index+1]] = True
+                    index += 1
+                elif event.key == pygame.K_UP and index>0:
+                    hand_rect.y -= 28.5
+                    menu_dict[menu[index]] = False
+                    menu_dict[menu[index-1]] = True
+                    index -= 1
+                elif event.key == pygame.K_RETURN:
+                    return menu_dict
+        pygame.display.update()
+        clock.tick(60)
+        
+    
+    
+
+
+menu = welcome(screen)
+GAME_ACTIVE = menu["PlayGame"]
 # Gameloop
 while True :
     collide_left = False
     collide_right = False
-
+    scroll = 100
+    offset_y = 0
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT]:
         # Checking collision with left wall so that player can't go out of screen
         if player_rect.left < wallList[3].right:
             collide_left = True
-            print("collided with left wall")
+            # print("collided with left wall")
         else:
             player_rect.x-=5
     if keys[pygame.K_RIGHT]:
         # Checking collision with right wall so that player can't go out of screen
         if player_rect.right > wallList[0].left:
             collide_right = True
-            print("collided with right wall")
+            # print("collided with right wall")
         else:
             player_rect.x+=5
     # Events loop
-    for event in pygame.event.get() :
-        if event.type == pygame.QUIT:
+    for event in pygame.event.get()  :
+        if event.type == pygame.QUIT or (menu["Exit"]):
             pygame.quit()
             exit()
         if event.type == pygame.KEYDOWN:
@@ -222,7 +314,7 @@ while True :
 
         # Player Movements and Logic
         PLAYER_GRAVITY += 0.7
-        player_rect.y += PLAYER_GRAVITY   
+        player_rect.y += (PLAYER_GRAVITY+offset_y)  
         if (player_rect.bottom >= WIN_HEIGHT-floor_surf.get_height()) and not PLAYER_THIRDWAY:
             player_rect.bottom = WIN_HEIGHT-floor_surf.get_height()
             PLAYER_GRAVITY = 0
@@ -244,10 +336,13 @@ while True :
         if player_rect.bottom < WIN_HEIGHT*(1/3):
             PLAYER_THIRDWAY = True
         if PLAYER_THIRDWAY and player_rect.top > 100:
-            FLOOR_VELOCITY = 1 
-        elif (player_rect.top < 50)  :
-            PLAYER_GRAVITY += min(1, 0.5*(50-player_rect.top))
-            FLOOR_VELOCITY = 3 
+            FLOOR_VELOCITY = 1
+        elif (player_rect.top < scroll):
+            player_rect.top = scroll
+            offset_y = -PLAYER_GRAVITY
+            FLOOR_VELOCITY = -(PLAYER_GRAVITY)
+
+             
         # else:
         #     FLOOR_VELOCITY  = 2
         # print(pygame.sprite.Sprite.groups(Floor()))
